5c6392695de0f09db33651efcc1358a774d29b53
Rusland420 <rzfromyrizk@gmai.com>
2022-08-11 01:52:09 +0300

    new

--- src/index.js
+++ src/index.js
@@ -35,11 +35,67 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '*': ' ',
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr=[]
+    let morse=[]
+    let morse2=''
+
+for( let i = 0;i<expr.length;i=i+10){
+    arr.push(expr.slice(i,(i+10)))
 }
+
+for(let i=0;i<arr.length;i++){
+    
+    if(arr[i]==='**********'){
+        morse.push('*')
+    }
+    else{
+       for(let j=0;j<arr[i].length;j=j+2){
+        morse.push(arr[i].slice(j,(j+2)))
+       }
+    }
+    morse.push(' ')
+}
+
+for(let i=0;i<morse.length;i++){
+    if(morse[i]==='00'){
+       morse2=morse2+''
+    }
+    if(morse[i]==='10'){
+        morse2= morse2+'.'
+    }
+    if(morse[i]==='11'){
+        morse2= morse2+'-'
+    }
+    if(morse[i]==='*'){
+        morse2=morse2+'*'
+    }
+    if(morse[i]===' '){
+        morse2=morse2+' '
+    }
+}
+let arrResult=morse2.split(' ')
+
+let result=''
+let keys=Object.keys(MORSE_TABLE);
+let values = Object.values(MORSE_TABLE);
+arrResult.forEach(element => {
+     for (let i=0;i<keys.length;i++){
+        if(element === keys[i]){
+            result=result+values[i]
+        }
+     }
+    }
+)
+
+return result
+
+}
+   
+
 
 module.exports = {
     decode
